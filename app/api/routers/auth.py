f9007a2a8b63d68a53eeb654b99ec50e7a4b9c1d
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-11-09 23:13:33 +0100

    etsted

--- app/api/routers/auth.py
+++ app/api/routers/auth.py
@@ -74,13 +74,6 @@ async def register_user(
     Returns:
         RegisterResponseModel: Status, message, and created user data.
     """
-    stmt = select(User).where(User.username == user_in.username)
-    existing_user = await session.execute(stmt)
-    if existing_user.scalars().first():
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Username already exists"
-        )
 
     db_user = await user_repo.get_by_email(session, email=user_in.email)
     if db_user:
